f36e807397ff562571dd27470b8086f8f08fa2b0
Stella Miliatis <smiliati@trinity.edu>
2024-09-17 10:49:53 -0500

    updated

---
+++ ListNode.java
@@ -0,0 +1,22 @@
+public class ListNode {
+    //Fields
+    int val;
+    ListNode next;
+
+    //Constructors
+    ListNode() {};
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    //Methods
+    
+
+
+}
---
+++ linkListReverse.java
@@ -0,0 +1,5 @@
+public class linkListReverse {
+    public ListNode reverseList(ListNode head) {
+        
+    }
+}
