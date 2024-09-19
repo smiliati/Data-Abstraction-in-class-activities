dec6f604f83e2dd51691b27390bd97877e30e47a
Stella Miliatis <smiliati@trinity.edu>
2024-09-19 10:11:54 -0500

    updated

--- linkListReverse.java
+++ linkListReverse.java
@@ -1,6 +1,15 @@
 public class linkListReverse {
     //Method
-    public ListNode reverseList(ListNode head) {
+    public void reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next = null;
+        ListNode curr = head;
 
+        while (curr!= null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
     }
 }
