f555868c87ad5d44a61804da2797aa82c7f5aa47
Stella Miliatis <smiliati@trinity.edu>
2024-10-01 10:26:06 -0500

    updated

---
+++ sumStuff.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+
+public class sumStuff {
+    
+    //given an array of ints and a target value, return the two indeces of the two numbers in the array that would sum to the target
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i<nums.length; i++) {
+            int diff = target - nums[i];
+            if (numMap.containskey)
+        }
+    }
+
+}
---
+++ symmetricPairs.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class symmetricPairs {
+    //given an array of pairs, return the pairs that have symmetric pairs
+    public static void findSymPairs(int arr[][]) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int first = arr[i][0];
+            int sec = arr[i][1];
+
+            Integer val = hm.get(sec);
+
+            if (val != null && val == first) {
+                System.out.println(sec + " " + first);
+            } else {
+                hm.put(first, sec);
+            }
+        }
+    }
+}
