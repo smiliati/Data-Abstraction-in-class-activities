0327744aef137fe53e11323888956528240857eb
Stella Miliatis <smiliati@trinity.edu>
2024-10-08 10:20:29 -0500

    updated

--- Trees.java
+++ Trees.java
@@ -19,4 +19,39 @@ public class Trees {
         res.add(node.val);
         inorder(node.right, res);
     }
+
+    public Node deleteNode(Node root, int x) {
+        if (root == null) {
+            return root;
+        }
+
+        if (root.val > x) {
+            root.left = deleteNode(root.left, x);
+        } else if (root.val < x) {
+            root.right = deleteNode(root.right, x);
+        } else {
+            if (root.left == null && root.right == null) {
+                return null;
+            } else if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                Node replace = getSuccessor(root);
+                root.val = replace.val;
+                root.right = deleteNode(root.right, replace.val);
+            }
+        }
+
+        public Node getSuccessor(Node curr) {
+            curr = curr.right;
+            while (curr != null && curr.left != null) {
+                curr = curr.left;
+            }
+            return curr;
+        }
+
+
+    }
+
 }
