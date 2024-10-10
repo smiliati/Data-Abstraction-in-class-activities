6a9a344317de9364aff379307c82e75e92273008
Stella Miliatis <smiliati@trinity.edu>
2024-10-10 10:13:57 -0500

    updated

--- Trees.java
+++ Trees.java
@@ -108,9 +108,23 @@ public class Trees {
         return Math.max(leftHeight, rightHeight) + 1;
     }
 
-    //I WROTE:
-    public TreeNode sortedArrayToBST(int[] nums) {
-        
+    //DR TAN WROTE:
+    public Node sortedArrayToTree(int[] nums) {
+        int n = nums.length;
+        if (n==0) {
+            return null;
+        }
+        return helper(nums, 0, n-1);
+    }
+    public Node helper(int[] nums, int s, int e) {
+        if (s>e) {
+            return null;
+        }
+        int mid = s + (e-s) / 2;
+        Node node = new Node(nums[mid]);
+        node.left = helper(nums, s, mid - 1);
+        node.right = helper(nums, mid + 1, e);
+        return node;
     }
 
 }
