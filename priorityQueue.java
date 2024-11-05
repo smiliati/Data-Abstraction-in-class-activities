6aaaedb3ef15fbc0be76fbafc8ab3fb14e6f28a0
Stella Miliatis <smiliati@trinity.edu>
2024-11-05 10:35:35 -0600

    updated

---
+++ priorityQueue.java
@@ -0,0 +1,60 @@
+import java.util.PriorityQueue;
+
+public class priorityQueue {
+    
+    //MY CODE:
+    //in class exercise
+    //numberGame where Alice and Bob take turns removing the min element and adding the removed element to an array
+    public int[] numberGame(int[] nums) {
+        //initialize returned array
+        int[] result = new int[];
+
+        //loop over the array
+        for (int i = 0; i < nums.length/2; i++) {
+
+            //collect Alice's number
+            int AliceNum = nums.min();
+            //remove Alice's number from the array
+            nums.remove(AliceNum);
+
+            //collect Bob's number
+            int BobNum = nums.min();
+            //remove Bob's number from the array
+            nums.remove(BobNum);
+
+            //add Bob's number to the result
+            result.append(BobNum);
+            //add Alice's number to the result
+            result.append(AliceNum);
+        }
+
+        //return the result
+        return result;
+
+    }
+
+    //DR TAN'S CODE:
+    public int[] numGame(int[] arr) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int[] res = new int[arr.length];
+
+        for (int num : arr) {
+            queue.add(num);   
+        }
+
+        int index = 0;
+
+        while (!queue.isEmpty()) {
+            int alice = queue.remove();
+            int bob = queue.remove();
+
+            res[index] = bob;
+            index++;
+            res[index] = alice;
+            index++;
+        }
+
+        return res;
+    }
+
+}
